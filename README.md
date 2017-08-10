ffcf3ba070d0a3e4e913f9c9926d4be05dcd5579
NickyPan <xinchenpan@genencare.com>
2017-08-10 12:46:52 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Udacity前端项目 -- 订阅阅读器测试
+
+这是[Udacity](https://cn.udacity.com)前端开发（进阶）课程的第四个项目，利用Jasmine测试一个已完成的订阅阅读器。
+
+Quick Links:
+
+- **[Getting started](#getting-started)**
+- **[Testing overview](#testing-overview)**
+- **[Reference](#reference)**
+
+### Getting started
+
+#### Usage
+
+本项目暂未打包，直接在浏览器中运行index.html即可
+
+### Testing overview
+
+这里介绍了项目利用Jasmine测试的内容：
+
+- 测试是否定义了订阅源，且所有订阅源是否都有名称和订阅链接字段
+- 测试侧边栏菜单功能是否正常
+- 测试获取订阅内容功能是否正常，且切换新源时内容是否改变
+
+### Reference
+
+- [Jasmine](https://jasmine.github.io/)
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,122 @@
+/* feedreader.js
+ *
+ * 这是 Jasmine 会读取的spec文件，它包含所有的要在你应用上面运行的测试。
+ */
+
+/* 我们把所有的测试都放在了 $() 函数里面。因为有些测试需要 DOM 元素。
+ * 我们得保证在 DOM 准备好之前他们不会被运行。
+ */
+$(function() {
+
+    /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
+     * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
+    */
+
+    describe('RSS Feeds', function() {
+        /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
+         * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
+         * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
+         * 页面看看会发生什么。
+        */
+        it('allFeeds are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+
+        /* 测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
+         */
+         it('each feed has url which is be defined', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBe(undefined);
+            }
+        });
+
+        /* 测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
+         */
+         it('each feed has name which is be defined', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name).not.toBe(undefined);
+            }
+        });
+    });
+
+    describe('The menu', function() {
+        /* 这是一个叫做 "The menu" 的测试用例 ，用来测试菜单的情况。*/
+
+        /* 这个测试用例保证菜单元素默认是隐藏的。*/
+        it('menu is hidden', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+         /* 这个测试用例保证当菜单图标被点击的时候菜单会切换可见状态
+          * 假如菜单隐藏，点击菜单图标应该显示；
+          * 假如菜单显示，点击菜单图标应该隐藏；
+          *以上通过，则测试成功。
+          */
+        it('toggling menu icon can show/hide menu', function() {
+            var menuIcon = $('.menu-icon-link');
+            if ($('body').hasClass('menu-hidden')) {
+                $('.menu-icon-link').click();
+                expect($('body').hasClass('menu-hidden')).toBe(false);
+                console.log('menu has been shown');
+            }
+            if (!$('body').hasClass('menu-hidden')) {
+                $('.menu-icon-link').click();
+                expect($('body').hasClass('menu-hidden')).toBe(true);
+                console.log('menu has been hidden');
+            }
+        });
+    });
+
+    describe('Initial Entries', function() {
+        /* 这是一个叫做 "Initial Entries" 的测试用例, 用来测试数据被正确初始化 */
+
+        /* 测试保证 loadFeed 函数被调用而且工作正常；
+         * 并在 .feed 容器元素里面至少有一个 .entry 的元素。
+         */
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('loadFeed() is working well', function(done) {
+            expect($(".feed").find(".entry").length >= 1).toBeTruthy();
+            done();
+        });
+
+    });
+
+    describe('New Feed Selection', function() {
+        /* 这是一个叫做 "New Feed Selection" 的测试用例, 用来测试加载新源是否正常 */
+
+        /* 测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
+         */
+
+         var initialFeed, newFeed;
+
+         beforeEach(function(done) {
+            loadFeed(0, function() {
+                initialFeed = $( "div.feed" ).html();
+                loadFeed(1, function() {
+                    done();
+                });
+            });
+        });
+
+         it('Feed Selection has been changed', function(done) {
+            newFeed = $( "div.feed" ).html();
+            expect(newFeed).not.toBe(initialFeed);
+            done();
+        });
+
+        afterEach(function() {
+            init();
+        });
+
+    });
+
+}());
